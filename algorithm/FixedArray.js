765aa0dfbddfc5c2ffea7d266d525ec72ca9e2da
hayanLee <hayanlee.dev@gmail.com>
2024-05-28 11:14:11 +0900

    [240527] 알고리즘 과제

--- algorithm/FixedArray.js
+++ algorithm/FixedArray.js
@@ -61,42 +61,72 @@ export class FixedArray {
     //////      LEVEL 2      /////
     //////////////////////////////
     // 배열에서 특정 요소의 첫 번째 인덱스를 반환합니다. 요소가 없으면 -1을 반환합니다.
-    indexOf(searchElement) {}
+    indexOf(searchElement) {
+        for (let i = 0; i < this.#currentLength; i++) {
+            if (this.#array[i] === searchElement) return i;
+        }
+        return -1;
+    }
 
     // 배열의 각 요소에 대해 predicate 결과가 true인 요소 중 제일 첫번째 요소 1개만 반환
     // true 가 없으면 null 반환
-    find(predicate) {}
+    // [].find((item)=>item===1)
+    find(predicate) {
+        for (let i = 0; i < this.#currentLength; i++) {
+            if (predicate(this.#array[i])) return this.#array[i];
+        }
+        return null;
+    }
 
     // 배열의 각 요소에 대해 predicate 결과가 true인 요소 중 제일 첫번째 요소의 index반환
     // true 가 없으면 -1 반환
-    findIndex(predicate) {}
+    findIndex(predicate) {
+        for (let i = 0; i < this.#currentLength; i++) {
+            if (predicate(this.#array[i])) return i;
+        }
+        return -1;
+    }
 
     // 배열에 특정 요소가 포함되어 있는지 여부를 확인합니다. (true or false)
-    includes(searchElement) {}
+    includes(searchElement) {
+        return this.#array.indexOf(searchElement) !== -1;
+    }
 
     //////////////////////////////
     //////      LEVEL 3      /////
     //////////////////////////////
     // 배열의 각 요소에 대해 제공된 함수를 한 번씩 실행합니다.
-    forEach(callback) {}
+    forEach(callback) {
+        for (let i = 0; i < this.#currentLength; i++) {
+            callback(this.#array[i]);
+        }
+    }
 
-    // 배열의 각 요소에 대해 predicate 결과가 true인 요소를 모아 새로운 배열 반환
-    filter(predicate) {}
+    // 배열의 각 요소에 대해 predicate 결과가 true인 요소를 모아 새로운 배열 반환(FixedArray)
+    filter(predicate) {
+        const filteredArray = new FixedArray(this.#currentLength);
+        for (let i = 0; i < this.#currentLength; i++) {
+            if (predicate(this.#array[i])) filteredArray.push(this.#array[i]);
+        }
+        return filteredArray;
+    }
 
-    // 배열의 각 요소에 대해 callback 함수를 호출한 결과를 모아 새로운 배열로 반환
-    map(callback) {}
+    // 배열의 각 요소에 대해 callback 함수를 호출한 결과를 모아 새로운 배열로 반환(FixedArray)
+    map(callback) {
+        const mappedArray = new FixedArray(this.#currentLength);
+        for (let i = 0; i < this.#currentLength; i++) {
+            mappedArray.push(callback(this.#array[i]));
+        }
+        return mappedArray;
+    }
 
     // 배열의 각 요소에 대해 제공된 함수를 호출하여 누산기에 값을 축적
-    reduce(callback, initValue) {}
+    // [].reduce((a,c) => a+c, [])
+    reduce(callback, initValue) {
+        let acc = initValue; // 누적값 = 초기값
+        for (let i = 0; i < this.#currentLength; i++) {
+            acc = callback(acc, this.#array[i]);
+        }
+        return acc;
+    }
 }
-
-// const a = new FixedArray(3);
-// a.push(1);
-// a.push(2);
-// a.push(3);
-// console.log('길이 :' + a.getLength());
-// console.log(a.pop());
-// console.log(a.pop());
-// console.log('길이 :' + a.getLength());
-// console.log(a.stringify());
-// console.log(a);
