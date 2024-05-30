a6a38061733609df9ec53bc5cda976612bfdd257
hayanLee <hayanlee.dev@gmail.com>
2024-05-30 15:02:38 +0900

    del : project 별도 레포지토리로 이동

--- project/blog/src/pages/PostDetailPage/PostDetailPage.loader.js
+++
@@ -1,6 +0,0 @@
-export default async function postDetailPageLoader({ params }) {
-    const url = `https://jsonplaceholder.typicode.com/posts/${params.postId}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
--- project/blog/src/pages/PostsListPage/PostsListPage.loader.js
+++
@@ -1,6 +0,0 @@
-export default async function postListPageLoader() {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
